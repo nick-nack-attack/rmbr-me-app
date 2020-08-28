021a47a954283be7bc0dbb932004a10a5f3c3c60
Nick Farni <nickfarni001@gmail.com>
2020-08-28 11:28:36 -0600

    handling additional variables in env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,22 @@
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config({path: path.join(__dirname, '../.env')});
+
+const API_ENDPOINT = process.env.API_ENDPOINT;
+const TOKEN_KEY = process.env.TOKEN_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export {
+    API_ENDPOINT,
+    TOKEN_KEY,
+    API_KEY
+};
+
+/*
 export default {
-    API_ENDPOINT: `https://glacial-ocean-65957.herokuapp.com/api`,
+    API_ENDPOINT: 'localhost:8000/api' | `https://glacial-ocean-65957.herokuapp.com/api`,
     TOKEN_KEY: 'rmbrme-client-auth-token',
     API_KEY: process.env.REACT_APP_API_KEY,
 };
+*/
--- src/services/token-service.js
+++ src/services/token-service.js
@@ -1,5 +1,5 @@
 import jwtDecode from 'jwt-decode';
-import config from '../config';
+import * as config from '../config';
 
 let _timeoutId;
 const _TEN_SECONDS_IN_MS = 10000;

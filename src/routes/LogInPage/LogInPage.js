b14104b981552b4ad1a76f94b639eb39ad6dcb06
Nick Nack Attack <nickfarni001@gmail.com>
2022-04-08 19:36:04 -0600

    refactor

--- src/routes/LogInPage/LogInPage.js
+++ src/routes/LogInPage/LogInPage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import LogInForm from '../../components/LogInForm/LogInForm';
 
 export default class LogInPage extends Component {
-
     static defaultProps = {
         location: {},
         history: {

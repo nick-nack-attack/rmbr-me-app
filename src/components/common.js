c67490fc8aaeaee56bc5ccc892ee4870480bf0b5
Nick Nack Attack <nickfarni001@gmail.com>
2022-04-10 23:45:46 -0600

    add styled component

---
+++ src/components/common.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ListItem = styled.li`
+  display: inline-block;
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+  width: 90%;
+  max-width: 500px;
+  margin-top: 20px;
+`;
